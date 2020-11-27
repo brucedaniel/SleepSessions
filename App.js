78f02e19e53f478090c860a01b53ce7a93128a66
bruce <bdaniel3@gmail.com>
2020-11-26 21:38:40 -0800

    be more quiet

--- App.js
+++ App.js
@@ -26,6 +26,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+
 const App: () => React$Node = () => {
   return (
     <>
