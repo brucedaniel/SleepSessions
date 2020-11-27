abf9b03d34733a775c122ea6c6452d132cbbe728
bruce <bdaniel3@gmail.com>
2020-11-26 16:12:44 -0800

    yarn add react-native-responsive-linechart

--- App.js
+++ App.js
@@ -35,6 +35,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { Chart, VerticalAxis, HorizontalAxis, Line } from 'react-native-responsive-linechart'
+
+
 import { Dimensions } from "react-native";
 const screenWidth = Dimensions.get("window").width
 
@@ -107,13 +110,13 @@ const App: () => React$Node = () => {
 
           <Carousel>
     <View style={carouselStyle.slide1}>
-      <UserCard url={userURLs[0]}></UserCard>
+      <UserCard url={userURLs[0]} key='Addy'></UserCard>
     </View>
     <View style={carouselStyle.slide2}>
-      <UserCard url={userURLs[1]}></UserCard>
+      <UserCard url={userURLs[1]} key='Brit'></UserCard>
     </View>
     <View style={carouselStyle.slide3}>
-    <UserCard url={userURLs[2]}></UserCard>
+    <UserCard url={userURLs[2]} key='Brit'></UserCard>
     </View>
   </Carousel>
 
@@ -288,6 +291,31 @@ export class UserCard extends React.Component<Props> {
           chartConfig={chartConfig}
           hideLegend={false}
       />
+      <Chart
+  style={{ height: 200, width: 400 }}
+  data={[
+    { x: -2, y: 15 },
+    { x: -1, y: 10 },
+    { x: 0, y: 12 },
+    { x: 1, y: 7 },
+    { x: 2, y: 6 },
+    { x: 3, y: 8 },
+    { x: 4, y: 10 },
+    { x: 5, y: 8 },
+    { x: 6, y: 12 },
+    { x: 7, y: 14 },
+    { x: 8, y: 12 },
+    { x: 9, y: 13.5 },
+    { x: 10, y: 18 },
+  ]}
+  padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+  xDomain={{ min: -2, max: 10 }}
+  yDomain={{ min: 0, max: 20 }}
+>
+  <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+  <HorizontalAxis tickCount={5} />
+  <Line theme={{ stroke: { color: '#ffa502', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+</Chart>
 
        </>
       )
